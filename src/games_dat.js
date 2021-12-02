7c3e28dc5302a7e1261512bbfe229f25c3a5e5e9
Tom H <tom@auroriax.com>
2021-12-02 21:47:22 +0100

    Removes leading underscore because these aren't served by Github pages

--- src/games_dat.js
+++ src/games_dat.js
@@ -1,56 +1,56 @@
 var games_gallery = [
   {
     "url": "https://auroriax.itch.io/gridblocked",
-    "thumb": "_plus_gridblocked.png",
+    "thumb": "plus_gridblocked.png",
     "title": "Gridblocked",
     "author": "Auroriax",
     "homepage": "https://auroriax.itch.io/gridblocked"
   },
   {
     "url": "https://minotalen.itch.io/bodenhorizont",
-    "thumb": "_plus_bodenhorizont.png",
+    "thumb": "plus_bodenhorizont.png",
     "title": "Bodenhorizont",
     "author": "minotalen",
     "homepage": "https://minotalen.itch.io/"
   },
   {
     "url": "https://minotalen.itch.io/pipetwist",
-    "thumb": "_plus_pipetwist.png",
+    "thumb": "plus_pipetwist.png",
     "title": "Pipe Twist",
     "author": "minotalen",
     "homepage": "https://minotalen.itch.io/"
   },
   {
     "url": "https://minotalen.itch.io/crystalanomaly",
-    "thumb": "_plus_anomaly.png",
+    "thumb": "plus_anomaly.png",
     "title": "Crystal Anomaly",
     "author": "minotalen",
     "homepage": "https://minotalen.itch.io/"
   },
   {
     "url": "https://boredmatt.itch.io/line-world",
-    "thumb": "_plus_lineworld.gif",
+    "thumb": "plus_lineworld.gif",
     "title": "Line World",
     "author": "BoredMatt",
     "homepage": "https://boredmatt.itch.io/"
   },
   {
     "url": "https://boredmatt.itch.io/beams-and-flowers-2",
-    "thumb": "_plus_beamsflowers.gif",
+    "thumb": "plus_beamsflowers.gif",
     "title": "Beams and Flowers 2",
     "author": "BoredMatt",
     "homepage": "https://boredmatt.itch.io/"
   },
   {
     "url": "https://boredmatt.itch.io/hex-road-blocks",
-    "thumb": "_plus_hexroadblocks.gif",
+    "thumb": "plus_hexroadblocks.gif",
     "title": "Hex Road Blocks",
     "author": "BoredMatt",
     "homepage": "https://boredmatt.itch.io/"
   },
   {
     "url": "https://vexorian.itch.io/kye-ps",
-    "thumb": "_plus_kye.png",
+    "thumb": "plus_kye.png",
     "title": "Kye.ps",
     "author": "vexorian",
     "homepage": "https://vexorian.itch.io/"
