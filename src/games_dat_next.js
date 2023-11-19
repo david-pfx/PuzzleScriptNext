d71894a7aee2bccd31cab538b8308e5ee5d25ae7
david <david@polyomino.com>
2023-11-20 09:55:12 +1100

    Fix relative links in Gallery
    Update all links

--- src/games_dat_next.js
+++ src/games_dat_next.js
@@ -1,132 +1,132 @@
 var games_gallery = [
   {
-    "url": "/src/play.html?demo=plus/gridblocked",
+    "url": "play.html?demo=plus/gridblocked",
     "thumb": "plus_gridblocked.gif",
     "title": "Grid Blocked",
     "author": "Auroriax",
     "homepage": "https://auroriax.itch.io/"
   },
   {
-    "url": "/src/play.html?demo=plus/selenes_labyrinth",
+    "url": "play.html?demo=plus/selenes_labyrinth",
     "thumb": "plus_selenes_labyrinth.gif",
     "title": "Selene's Labyrinth",
     "author": "jcGyo",
     "homepage": "jcgyo.itch.io"
   },
   {
-    "url": "/src/play.html?demo=plus/sokobanana",
+    "url": "play.html?demo=plus/sokobanana",
     "thumb": "plus_sokobanana.gif",
     "title": "Sokobanana",
     "author": "Auroriax",
     "homepage": "https://auroriax.itch.io/"
   },
   {
-    "url": "/src/play.html?demo=plus/kye",
+    "url": "play.html?demo=plus/kye",
     "thumb": "plus_kye.gif",
     "title": "Kye.ps",
     "author": "vexorian",
     "homepage": "https://vexorian.itch.io/"
   },
   {
-    "url": "/src/play.html?p=ff57977aaf65938a33e7e224c807e2ef",
+    "url": "play.html?p=ff57977aaf65938a33e7e224c807e2ef",
     "thumb": "plus_stonehaven.gif",
     "title": "Stone Haven",
     "author": "bregehr",
     "homepage": ""
   },
   {
-    "url": "/src/play.html?p=669bd147a86d4011dcfafa6560172013",
+    "url": "play.html?p=669bd147a86d4011dcfafa6560172013",
     "thumb": "plus_sokobandragged.gif",
     "title": "Sokoban: Dragged",
     "author": "Jack Kutilek",
     "homepage": "https://jackkutilek.itch.io/"
   },
   {
-    "url": "/src/play.html?p=b7bd34b9e80fe29be270f01fc12ba35b",
+    "url": "play.html?p=b7bd34b9e80fe29be270f01fc12ba35b",
     "thumb": "plus_slideflipper.gif",
     "title": "SlideFlipper",
     "author": "MinoMe",
     "homepage": ""
   },
   {
-    "url": "/src/play.html?p=e1d2c64c57911816acb115766a3ec090",
+    "url": "play.html?p=e1d2c64c57911816acb115766a3ec090",
     "thumb": "plus_puzzlesquad.gif",
     "title": "PuzzleSquad",
     "author": "AlexB",
     "homepage": ""
   },
   {
-    "url": "/src/play.html?p=baf7a55d6879cf15362da9f4878f0890",
+    "url": "play.html?p=baf7a55d6879cf15362da9f4878f0890",
     "thumb": "plus_23lilypads.gif",
     "title": "23 Lily Pads",
     "author": "Karoo",
     "homepage": "https://karoo13.itch.io"
   },
   {
-    "url": "/src/play.html?demo=plus/magiciban_v1",
+    "url": "play.html?demo=plus/magiciban_v1",
     "thumb": "plus_magiciban.gif",
     "title": "Magiciban",
     "author": "Auroriax",
     "homepage": "https://auroriax.itch.io/"
   },
     {
-    "url": "/src/play.html?demo=patt/tubetransport",
+    "url": "play.html?demo=patt/tubetransport",
     "thumb": "patt_tubetransport.gif",
     "title": "Tube Transport",
     "author": "Noa",
     "homepage": ""
   },
   {
-    "url": "/src/play.html?demo=next/yasban",
+    "url": "play.html?demo=next/yasban",
     "thumb": "next_yasban.gif",
     "title": "Yet Another Sokoban",
     "author": "Davidus of Polyomino",
     "homepage": "https://david-pfx.github.io/PuzzleScriptNext/"
   },
   {
-    "url": "/src/play.html?demo=next/333",
+    "url": "play.html?demo=next/333",
     "thumb": "next_333.gif",
     "title": "333",
     "author": "Davidus of Polyomino",
     "homepage": "https://david-pfx.github.io/PuzzleScriptNext/"
   },
   {
-    "url": "/src/play.html?demo=next/2048",
+    "url": "play.html?demo=next/2048",
     "thumb": "next_2048.gif",
     "title": "2048",
     "author": "Davidus of Polyomino",
     "homepage": "https://david-pfx.github.io/PuzzleScriptNext/"
   },
   {
-    "url": "/src/play.html?demo=next/black_box",
+    "url": "play.html?demo=next/black_box",
     "thumb": "next_blackbox.gif",
     "title": "Black Box",
     "author": "Davidus of Polyomino",
     "homepage": "https://david-pfx.github.io/PuzzleScriptNext/"
   },
   {
-    "url": "/src/play.html?demo=next/bridges",
+    "url": "play.html?demo=next/bridges",
     "thumb": "next_bridges.gif",
     "title": "Bridges",
     "author": "Davidus of Polyomino",
     "homepage": "https://david-pfx.github.io/PuzzleScriptNext/"
   },
   {
-    "url": "/src/play.html?demo=next/fifteen",
+    "url": "play.html?demo=next/fifteen",
     "thumb": "next_fifteen.gif",
     "title": "Fifteen",
     "author": "Davidus of Polyomino",
     "homepage": "https://david-pfx.github.io/PuzzleScriptNext/"
   },
   {
-    "url": "/src/play.html?demo=next/netgame",
+    "url": "play.html?demo=next/netgame",
     "thumb": "next_netgame.gif",
     "title": "Net Game",
     "author": "Davidus of Polyomino",
     "homepage": "https://david-pfx.github.io/PuzzleScriptNext/"
   },
   {
-    "url": "/src/play.html?demo=next/colour_chart",
+    "url": "play.html?demo=next/colour_chart",
     "thumb": "next_colourchart.png",
     "title": "Colour Chart",
     "author": "Davidus of Polyomino",
