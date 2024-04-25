6e6f8086d24413962741352bd23142b29a1757f4
Henri Manson <h.manson@aal-audio.com>
2024-04-25 20:40:08 +0200

    svg template experiment

--- src/js/graphics.js
+++ src/js/graphics.js
@@ -67,6 +67,58 @@ function createSVGSprite(name, vector) {
     return canvas;
 }
 
+
+
+// Create and return a SVG template sprite canvas
+function createSVGTemplateSprite(name, vector) {
+    var canvas = makeSpriteCanvas(name);
+    if (vector.w) canvas.width *= vector.w;
+    if (vector.h) canvas.height *= vector.h;
+    var context = canvas.getContext('2d');
+    const body = vector.data.join("\n");
+    const svg =
+`<svg
+   viewBox="0 0 100 100"
+   xmlns="http://www.w3.org/2000/svg"
+    xmlns:xlink="http://www.w3.org/1999/xlink"
+>
+  <defs>
+    <path id="arrow"
+      d="M 20 50 L 80 50 M 60 30 L 80 50 M 60 70 L 80 50" 
+      stroke-width="12"
+      stroke-linecap="round"
+      stroke="currentColor"
+    />
+   <path id="player"
+     d="M 5 5 L 95 5 L 95 95 L 5 95 Z" 
+     fill="none"
+     stroke="blue"
+     stroke-width="5"
+     stroke-linecap="round"
+    />
+    <path id="goal"
+     d="M 5 5 L 95 5 L 95 95 L 5 95 Z" 
+     fill="none"
+     stroke="blue"
+     stroke-width="3" 
+     stroke-dasharray="5,5"
+    />
+  </defs>
+  ${body}
+</svg>`;
+    var blob = new Blob([svg], {type: 'image/svg+xml'});
+    var url = URL.createObjectURL(blob);
+    var image = document.createElement('img');
+    image.src = url;
+    image.addEventListener('load', function () {
+        context.drawImage(image, 0, 0);
+        URL.revokeObjectURL(url);
+        redraw();
+    }, false);
+    return canvas;
+}
+
+
 // draw the pixels of the sprite grid data into the context at a cell position, 
 function renderSprite(context, spritegrid, colors, padding, x, y, size) {
     colors ||= ['#00000000', state.fgcolor];
@@ -140,7 +192,9 @@ var editor_s_grille=[[0,1,1,1,0],[1,0,0,0,0],[0,1,1,1,0],[0,0,0,0,1],[0,1,1,1,0]
 var spriteImages;
 
 function handleVector(name, vector) {
-    return vector.type === 'canvas' ? createJsonSprite(name, vector) : createSVGSprite(name, vector);
+    return vector.type === 'canvas' ? createJsonSprite(name, vector)
+    : vector.type === 'svg' ? createSVGSprite(name, vector)
+    : createSVGTemplateSprite(name, vector);
 }
 
 function regenSpriteImages() {
