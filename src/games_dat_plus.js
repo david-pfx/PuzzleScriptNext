37b9f48f8e7e52c282fa88c9dabb88f5ac1d1625
david <david@polyomino.com>
2024-09-06 13:38:49 +1000

    Filter gallery
    Keep only hackable in demo or with gist
    Import pip_twist to fix parsing

--- src/games_dat_plus.js
+++ src/games_dat_plus.js
@@ -1,205 +1,135 @@
 var games_gallery_plus = [
   {
-    "url": "https://auroriax.itch.io/gridblocked",
+    "url": "play.html?demo=plus/gridblocked",
     "thumb": "plus_gridblocked.png",
     "title": "Gridblocked",
     "author": "Auroriax",
     "homepage": "https://auroriax.itch.io/"
   },
   {
-    "url": "https://minotalen.itch.io/bodenhorizont",
-    "thumb": "plus_bodenhorizont.png",
-    "title": "Bodenhorizont",
-    "author": "minotalen",
-    "homepage": "https://minotalen.itch.io/"
-  },
-  {
-    "url": "https://minotalen.itch.io/pipetwist",
+    "url": "play.html?demo=plus/pipe_twist",
     "thumb": "plus_pipetwist.png",
     "title": "Pipe Twist",
     "author": "minotalen",
     "homepage": "https://minotalen.itch.io/"
   },
   {
-    "url": "https://minotalen.itch.io/crystalanomaly",
-    "thumb": "plus_anomaly.png",
-    "title": "Crystal Anomaly",
-    "author": "minotalen",
-    "homepage": "https://minotalen.itch.io/"
-  },
-  {
-    "url": "https://boredmatt.itch.io/line-world",
+    "url": "play.html?p=535e38ff313a18eba945b41f5667523a",
     "thumb": "plus_lineworld.gif",
     "title": "Line World",
     "author": "BoredMatt",
     "homepage": "https://boredmatt.itch.io/"
   },
   {
-    "url": "https://boredmatt.itch.io/beams-and-flowers-2",
-    "thumb": "plus_beamsflowers.gif",
-    "title": "Beams and Flowers 2",
-    "author": "BoredMatt",
-    "homepage": "https://boredmatt.itch.io/"
-  },
-  {
-    "url": "https://boredmatt.itch.io/hex-road-blocks",
+    "url": "play.html?p=a02a1dbed19dbd7a69576b6788a43891",
     "thumb": "plus_hexroadblocks.gif",
     "title": "Hex Road Blocks",
     "author": "BoredMatt",
     "homepage": "https://boredmatt.itch.io/"
   },
   {
-    "url": "https://vexorian.itch.io/kye-ps",
+    "url": "play.html?demo=plus/kye",
     "thumb": "plus_kye.png",
     "title": "Kye.ps",
     "author": "vexorian",
     "homepage": "https://vexorian.itch.io/"
   },
   {
-    "url": "https://stingby12.itch.io/simple-parallel-universe-game",
+    "url": "play.html?p=c65aa0e6f030328728b878448ae1ade2",
     "thumb": "plus_spug.png",
     "title": "Simple Parallel Universe Game",
     "author": "Stingby12",
     "homepage": "https://stingby12.itch.io/"
   },
   {
-    "url": "https://auroriax.github.io/PuzzleScript/play.html?p=ff57977aaf65938a33e7e224c807e2ef",
+    "url": "play.html?p=ff57977aaf65938a33e7e224c807e2ef",
     "thumb": "plus_stonehaven.png",
     "title": "Stone Haven",
     "author": "bregehr",
     "homepage": ""
   },
   {
-    "url": "https://auroriax.github.io/PuzzleScript/play.html?p=669bd147a86d4011dcfafa6560172013",
+    "url": "play.html?p=669bd147a86d4011dcfafa6560172013",
     "thumb": "plus_sokobandragged.png",
     "title": "Sokoban: Dragged",
     "author": "Jack Kutilek",
     "homepage": "https://jackkutilek.itch.io/"
   },
   {
-    "url": "https://auroriax.github.io/PuzzleScript/play.html?p=b7bd34b9e80fe29be270f01fc12ba35b",
+    "url": "play.html?p=b7bd34b9e80fe29be270f01fc12ba35b",
     "thumb": "plus_slideflipper.png",
     "title": "SlideFlipper",
     "author": "MinoMe",
     "homepage": ""
   },
   {
-    "url": "https://jackkutilek.itch.io/conways-game-of-sokoban",
-    "thumb": "plus_conwaysokoban.gif",
-    "title": "Conway's Game of Sokoban",
-    "author": "Jack Kutilek",
-    "homepage": "https://jackkutilek.itch.io/"
-  },
-  {
-    "url": "https://auroriax.itch.io/ad-infinitum",
+    "url": "play.html?p=01fbb58cf41f707e5f440e7b947c9d63",
     "thumb": "plus_adinfinitum.png",
     "title": "Ad Infinitum",
     "author": "Auroriax",
     "homepage": "https://auroriax.itch.io/"
   },
   {
-    "url": "https://rh9.itch.io/block-switch",
-    "thumb": "plus_blockswitch.png",
-    "title": "Block Switch",
-    "author": "RH9",
-    "homepage": "https://rh9.itch.io/"
-  },
-  {
-    "url": "http://www.clickmazes.com/boxup/new-boxup.htm",
+    "url": "play.html?p=192b7403618c6c10eccb83986283053b",
     "thumb": "plus_boxup.png",
     "title": "BoxUp",
     "author": "Andrea Gilbert (clickmazes)",
     "homepage": "http://www.clickmazes.com/index.htm"
   },
   {
-    "url": "https://auroriax.itch.io/magiciban",
+    "url": "play.html?p=d96dc0f496fabac68ffc9fa4ee2ea7ad",
     "thumb": "plus_magiciban.png",
     "title": "Magiciban",
     "author": "Auroriax",
     "homepage": "https://auroriax.itch.io/"
   },
   {
-    "url": "https://jackkutilek.itch.io/magicians-cat",
-    "thumb": "plus_magicianscat.png",
-    "title": "Magician's Cat",
-    "author": "Jack Kutilek",
-    "homepage": "https://jackkutilek.itch.io/"
-  },
-  {
-    "url": "https://competor.itch.io/magicians-hat-witchs-hat",
+    "url": "play.html?p=9fb3e0fd18c6131e55be0085ab67100f",
     "thumb": "plus_magicianwitchhat.png",
     "title": "Magician's hat Witch's hat",
     "author": "competor",
     "homepage": "https://competor.itch.io/"
   },
   {
-    "url": "https://competor.itch.io/cube",
+    "url": "play.html?p=e0370bec295f139da8493ab96407517c",
     "thumb": "plus_competorcube.png",
     "title": "Cube",
     "author": "competor",
     "homepage": "https://competor.itch.io/"
   },
   {
-    "url": "https://mariomak967.itch.io/stars-align",
-    "thumb": "plus_starsalign.png",
-    "title": "Stars Align",
-    "author": "MarioMak967",
-    "homepage": "https://mariomak967.itch.io/"
-  },
-  {
-    "url": "https://auroriax.github.io/PuzzleScript/play.html?p=e1d2c64c57911816acb115766a3ec090",
+    "url": "play.html?p=e1d2c64c57911816acb115766a3ec090",
     "thumb": "plus_puzzlesquad.png",
     "title": "PuzzleSquad",
     "author": "AlexB",
     "homepage": ""
   },
   {
-    "url": "https://competor.itch.io/copernicus",
+    "url": "play.html?p=0157f0071baf961c5e0f6405bc6e399c",
     "thumb": "plus_copernicus.png",
     "title": "Copernicus",
     "author": "competor",
     "homepage": "https://competor.itch.io"
   },
   {
-    "url": "https://au-voleur.itch.io/microban-1",
+    "url": "play.html?p=dcbd9ff6eff2d1d352a982cdf0bc0817",
     "thumb": "plus_skinnermicroban1.png",
     "title": "Microban I",
     "author": "au voleur (levels by David W. Skinner)",
     "homepage": "https://au-voleur.itch.io"
   },
   {
-    "url": "https://auroriax.github.io/PuzzleScript/play.html?p=baf7a55d6879cf15362da9f4878f0890",
+    "url": "play.html?p=baf7a55d6879cf15362da9f4878f0890",
     "thumb": "plus_23_lilypads.png",
     "title": "23 Lily Pads",
     "author": "Karoo",
     "homepage": "https://karoo13.itch.io"
   },
   {
-    "url": "https://competor.itch.io/cookies-overlap-delicious",
+    "url": "play.html?p=f78ce963e0774299f589a482632e7256",
     "thumb": "plus_cookiesoverlapdelicious.png",
     "title": "Cookies, Overlap, Delicious!",
     "author": "competor",
     "homepage": "https://competor.itch.io/"
-  },
-  {
-    "url": "https://thinkycollective.itch.io/lab-rational-thinkings",
-    "thumb": "plus_labrat.png",
-    "title": "Lab Rat-ional Thinking",
-    "author": "Thinky Collective",
-    "homepage": "https://thinkycollective.itch.io/"
-  },
-  {
-    "url": "https://thinkycollective.itch.io/flying-crowbar-factory-simulator-2022",
-    "thumb": "plus_crowbar.png",
-    "title": "Flying Crowbar Factory Simulator 2022",
-    "author": "Thinky Collective",
-    "homepage": "https://thinkycollective.itch.io/"
-  },
-  {
-    "url": "https://karoo13.itch.io/sleight-of-hat",
-    "thumb": "plus_sleightofhat.png",
-    "title": "Sleight of Hat",
-    "author": "Karoo",
-    "homepage": "https://karoo13.itch.io"
   },
 ]
