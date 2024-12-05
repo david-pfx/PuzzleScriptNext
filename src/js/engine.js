44570992edeec5c1fdbad6f15abd8204e106dce9
david <david@polyomino.com>
2024-12-05 23:10:38 +1100

    Bug when undoing a restart into an again loop (PS) #141
    PS #1026

--- src/js/engine.js
+++ src/js/engine.js
@@ -1382,6 +1382,9 @@ function DoRestart(force) {
 	if (force!==true && ('norestart' in state.metadata)) {
 		return;
 	}
+	if (againing){
+		DoUndo(force,true);
+	}
 	restarting=true;
 	if (force!==true) {
 		addUndoState(backupLevel());
