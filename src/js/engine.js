499ebc250f3117a316c71a4f3b6d8c5b4403af61
david <david@polyomino.com>
2025-04-09 23:52:17 +1000

    Fix off by one error preventing a restart of last level
    Test program

--- src/js/engine.js
+++ src/js/engine.js
@@ -97,6 +97,7 @@ function doSetupTitleScreenLevelContinue(){
             if (storage_has(document.URL+'_checkpoint')) {
                 var backupStr = storage_get(document.URL+'_checkpoint');
                 curlevelTarget = JSON.parse(backupStr);
+				if (debugSwitch.includes('menu')) console.log(`doSetupTitleScreenLevelContinue `, 'curlevelTarget=', curlevelTarget);
                 
                 var arr = [];
                 for(var p in Object.keys(curlevelTarget.dat)) {
@@ -124,8 +125,8 @@ function hasStartedTheGame() {
 }
 
 function hasFinishedTheGame() {
-	return state.metadata.level_select && solvedSections.length == state.sections.length
-		|| curLevelNo >= state.levels.length - 1; 
+	return state.metadata.level_select && (solvedSections.length == state.sections.length)
+		|| curLevelNo >= state.levels.length; 
 }
 
 function hasSolvedAtLeastOneSection() {
@@ -761,7 +762,7 @@ function loadLevelFromStateTarget(state,levelindex,target,randomseed) {
 }
 
 function loadLevelFromState(state,levelindex,randomseed) {  
-	if (debugSwitch.includes('load')) console.log(`loadLevelFromState(${levelindex})`);
+	if (debugSwitch.includes('load')) console.log(`loadLevelFromState(levelindex=${levelindex})`);
 	var leveldat = state.levels[levelindex];    
 	curLevelNo=levelindex;
 	curlevelTarget=null;
@@ -2071,6 +2072,7 @@ function cellRowMatchesWildcardFunctionGenerate(direction,cellRow,i, maxk, mink)
 
 }
 
+//@@ should these go in globals? Might fix one notified problem (no repro)
 let MOV_BITS = 5;		// doc: no of bits to hold movement as mask
 let MOV_MASK = 0x1f;	// doc: bit mask to match
 var STRIDE_OBJ = 1;	    // doc: size of BitVec to hold objects, at 32 bits per
@@ -3867,7 +3869,7 @@ function loadLevelFromStateOrTarget() {
 }
 
 function goToTitleScreen(){
-	if (debugSwitch.includes('load')) console.log(`gotoTitleScreen()`);
+	if (debugSwitch.includes('load')) console.log(`gotoTitleScreen() curlevelTarget=`, curlevelTarget, ` restartTarget=`, restartTarget);
     againing=false;
 	messagetext="";
 	statusText = "";
@@ -3897,7 +3899,7 @@ function resetFlickDat() {
 function updateLocalStorage() {
 	if (linkStack.length > 0)
 		return;
-	if (debugSwitch.includes('menu')) console.log(`updateLocalStorage`, curlevelTarget);
+	if (debugSwitch.includes('menu')) console.log(`updateLocalStorage`, 'curlevelTarget=', curlevelTarget, 'restartTarget=', restartTarget, 'curLevelNo=', curLevelNo);
 	try {
 		
 		storage_set(document.URL,curLevelNo);
